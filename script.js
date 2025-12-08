7d4687e35ebc150879104e9c0f09d0387e0f791d
Gigio0101 <gigio-86@libero.it>
2025-12-08 20:54:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded",()=>{
-  const todayBtn = document.getElementById("today-btn");
-  todayBtn.addEventListener("click",()=>location.reload());
-  document.getElementById("day-card").innerHTML = "<p>Funzionalità completa in versione premium caricata.</p>";
-});
